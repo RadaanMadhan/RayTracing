a599662641465686c13a4faa564170a8c2ba4630
RadaanMadhan <madhanradaan@gmail.com>
2025-09-13 22:29:49 +0200

    RT-13 : Create Camera clas

---
+++ src/core/Camera.cpp
@@ -0,0 +1,45 @@
+//
+// Created by Radaa on 13/09/2025.
+//
+
+#include "Camera.h"
+
+#include <cmath>
+
+Camera::Camera(const Vec3 origin, const Vec3 lookAt, const Vec3 up, const float fov, const float aspect) {
+    this->origin = origin;
+    this->forwardVector = (lookAt - origin).normalize();
+    this->rightVector = forwardVector.cross(up).normalize();
+    this->upVector = forwardVector.cross(rightVector).normalize();
+    this->fov = fov;
+    this->aspect = aspect;
+    const float height = 2 * std::tan(fov / 2); //Setting d = 1
+    const float width = aspect * height;
+    this->horizontalSpan = rightVector * width;
+    this->verticalSpan = upVector * height;
+    this->lowerLeftCorner = origin + forwardVector - horizontalSpan / 2 - verticalSpan / 2; //Setting d = 1
+}
+Ray Camera::getRay(const float u, const float v) const {
+    return {origin, u * horizontalSpan + v * verticalSpan + lowerLeftCorner};
+}
+void Camera::setFov(const float fov) {
+    const float height = 2 * std::tan(fov / 2);
+    const float width = aspect * height;
+    this->fov = fov;
+    this->horizontalSpan = rightVector * width;
+    this->verticalSpan = upVector * height;
+    this->lowerLeftCorner = origin + forwardVector - horizontalSpan / 2 - verticalSpan / 2;
+}
+Vec3 Camera::getOrigin() const {
+    return origin;
+}
+Vec3 Camera::getDirection() const {
+    return forwardVector;
+}
+float Camera::getViewportHeight() const {
+    return 2 * std::tan(this->fov / 2);
+}
+float Camera::getViewportWidth() const {
+    return aspect * 2 * std::tan(this->fov / 2);
+}
+
---
+++ src/core/Camera.h
@@ -0,0 +1,35 @@
+//
+// Created by Radaa on 13/09/2025.
+//
+
+#ifndef CAMERA_H
+#define CAMERA_H
+
+#include <vector>
+
+#include "Vec3.h"
+#include "Ray.h"
+
+class Camera {
+private:
+  Vec3 origin;
+  Vec3 forwardVector;
+  Vec3 rightVector;
+  Vec3 upVector;
+  float fov;
+  float aspect;
+  Vec3 horizontalSpan;
+  Vec3 verticalSpan;
+  Vec3 lowerLeftCorner;
+public:
+  Camera(Vec3 origin, Vec3 lookAt, Vec3 up, float fov, float aspect);
+  [[nodiscard]] Ray getRay(float u, float v) const;
+  void setFov(float fov);
+
+  [[nodiscard]] Vec3 getOrigin() const;
+  [[nodiscard]] Vec3 getDirection() const;
+  [[nodiscard]] float getViewportHeight() const;
+  [[nodiscard]] float getViewportWidth() const;
+};
+
+#endif //CAMERA_H
